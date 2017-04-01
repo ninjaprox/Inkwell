10b494c92004c84b20bf3537c2b41b005bb1c690
Vinh Nguyen <ninjaprox@gmail.com>
2017-04-01 21:44:27 +0800

    Update example

--- Example/Inkwell/ViewController.swift
+++ Example/Inkwell/ViewController.swift
@@ -2,23 +2,43 @@
 //  ViewController.swift
 //  Inkwell
 //
-//  Created by Vinh Nguyen on 03/30/2017.
-//  Copyright (c) 2017 Vinh Nguyen. All rights reserved.
+// Copyright (c) 2017 Vinh Nguyen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
 import UIKit
+import Inkwell
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let inkwell = Inkwell.shared
 
+        inkwell.APIKey = ""
+        inkwell.font(for: Font(family: "Indie Flower", variant: .regular),
+                     size: 25) { font in
+                        self.label.font = font
+                        self.label.text = font.fontName
+        }
+    }
 }
-
